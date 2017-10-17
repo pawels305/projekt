67da540780dfca4b65d0198dc54db5c70e34a20e
pawels305 <pawels305@wp.pl>
2017-10-17 16:48:56 +0200

    Update HelloWorld.java

--- PracowaniaProgramowania/src/main/java/example/HelloWorld.java
+++ PracowaniaProgramowania/src/main/java/example/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
     final static Logger logger = Logger.getLogger(HelloWorld.class);
 
     public static void main(String [ ] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World1");
 
         logger.info("To już drugie zajęcia");
     }
